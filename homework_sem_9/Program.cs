b45aa92cd49b6b3ba5f72eeaaf1a3b4191ffa01e
Dmitrii Lavrov <amidnik@rambler.ru>
2022-07-27 18:56:36 +0300

    homework_sem_9

--- homework_sem_9/Program.cs
+++ homework_sem_9/Program.cs
@@ -20,36 +20,40 @@
 
 
 //Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// int SumRecusiveType(int M, int N)
-// {
-//     if (M == N)  return M;
-//     else return  M + SumRecusiveType(M + 1, N);
-// }
-// Console.Clear();
-// Console.Write("Введите M: ");
-// int M = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-// SumRecusiveType(M, N);
-// Console.WriteLine(SumRecusiveType(M, N));
+int SumRecusiveType(int M, int N)
+{
+    if (M == N) return M;
+    else return M + SumRecusiveType(M + 1, N);
+}
+Console.Clear();
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M > N) Console.WriteLine("Введены некорректные числа: M должно быть меньше N");
+else
+{
+    SumRecusiveType(M, N);
+    Console.WriteLine(SumRecusiveType(M, N));
+}
 
 
 //----------------------
 
 
 //Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-int A(int n, int m)
-{
-  if (n == 0)
-    return m + 1;
-  else
-    if ((n != 0) && (m == 0))
-      return A(n - 1, 1);
-    else
-      return A(n - 1, A(n, m - 1));
-}
-Console.Write("Введите n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите m: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(A(n,m));
+// int A(int n, int m)
+// {
+//   if (n == 0)
+//     return m + 1;
+//   else
+//     if ((n != 0) && (m == 0))
+//       return A(n - 1, 1);
+//     else
+//       return A(n - 1, A(n, m - 1));
+// }
+// Console.Write("Введите n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(A(n,m));
